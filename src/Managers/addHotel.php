12086fa5f699556472096f17c67ff47b1be6e385
zaidmaslat <zaidmaslat.zm@gmail.com>
2023-03-07 01:25:10 +0300

    update style for add hotel

--- src/Managers/addHotel.php
+++ src/Managers/addHotel.php
@@ -1,46 +1,60 @@
-<style type="text/css">
-  .button {
-  background-color: #4CAF50;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-</style>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-echo "<h1 style=\"text-align:center; font-size:50px;\">Add Hotel</h1>";
-$managerID = $_GET['managerID'];
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/addHotel.css">
+</head>
 
-echo "<a href=\"/Managers/managersLoginPage.php?managerID=$managerID\" class=\"button\">Back to Home</a>" ; 
+<body>
 
-?>
+    <div>
+        <div class="registration-form">
+            <form action="/Managers/createHotel.php" method="POST">
+                <div class="form-group displayField ">
+                    <label class="marginLabel">Hotel Name</label>
+                    <input type="text" name="hotelName" class="form-control item">
+                </div>
+                <div class="form-group displayField">
+                    <?php 
+                              $managerID = $_GET['managerID'];
+                    
+                           echo  "<input type=\"text\" name=\"managerID\" value=\"$managerID\" style=\"display:none;\">"
+                        ?>
+                    <label class="marginLabel">Hotel-phone</label>
+                    <input type="text" name="hotelPhone" class="form-control item">
+                </div>
 
-<form action="/Managers/createHotel.php" method="POST">
-  <?php 
-          $managerID = $_GET['managerID'];
+                <div class="form-group displayField arrow">
+                    <label class="marginLabel">City</label>
+                    <select name="city" class="form-control item">
+                        <option value="Tafila">Tafila</option>
+                        <option value="Amman">Amman</option>
+                        <option value="Irbid">Irbid</option>
+                        <option value="Ajloun">Ajloun</option>
+                        <option value="Madaba">Madaba</option>
+                        <option value="Karak">Karak</option>
+                    </select>
+                </div>
 
-       echo  "<input type=\"text\" name=\"managerID\" value=\"$managerID\" style=\"display:none;\">"
-    ?>
-    Hotel Name:<br>
-    <input type="text" name="hotelName" value=""><br>
-    hotel-phone:<br>
-    <input type="text" name="hotelPhone" value=""><br>
-    City:<br>
-    <select name="city">
-      <option value="Tafila">Tafila</option>
-      <option value="Amman">Amman</option>
-      <option value="Irbid">Irbid</option>
-      <option value="Ajloun">Ajloun</option>
-      <option value="Madaba">Madaba</option>
-      <option value="Karak">Karak</option>
-    </select><br>
-  number-of-rooms:<br>
-  <input type="number" name="num_rooms" value=""><br>
-    <input type="submit" value="Submit">
-  </form> 
+                
+
+                <div class="form-group displayField">
+                    <label class="marginLabel">Number of rooms</label>
+                    <input type="number" name="num_rooms" class="form-control">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary create-account">Submit</button>
+                    </div>
+
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
