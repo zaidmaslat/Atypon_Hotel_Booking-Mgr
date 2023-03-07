b7746c62b5cb40d18820df146673a7abfde4b0cb
zaidmaslat <zaidmaslat.zm@gmail.com>
2023-03-07 23:41:40 +0300

    Update CSS for Manager scope

--- src/Managers/managersLoginPage.php
+++ src/Managers/managersLoginPage.php
@@ -24,7 +24,7 @@
 			<div class="collapse navbar-collapse" id="navbarNavDropdown">
 				<ul class="navbar-nav">
 					<li class="nav-item active">
-						<a class="nav-link" href="#">Home</a>
+						<a class="nav-link" href="/Managers/managersLoginPage.php?managerID=<?php echo $_GET["managerID"];?>">Home</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href='addHotel.php?managerID=<?php echo $_GET["managerID"];?>'>Add Hotel</a>
